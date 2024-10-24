5021c65876e2b93f89aece2b65df04868dd3e2b6
kenwoo9y <kenwoo9y@gmail.com>
2024-10-25 02:10:11 +0900

    installed SimpleCov

--- rails-app/spec/spec_helper.rb
+++ rails-app/spec/spec_helper.rb
@@ -12,6 +12,12 @@
 # the additional setup, and require it from the spec files that actually need
 # it.
 #
+
+require 'simplecov'
+SimpleCov.start 'rails' do
+  minimum_coverage 80 # カバレッジが80%未満の場合は失敗
+end
+
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
