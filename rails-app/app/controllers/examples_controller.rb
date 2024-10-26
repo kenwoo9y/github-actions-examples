5be8127311f1d8fd090a4451077348e12dcf0446
kenwoo9y <kenwoo9y@gmail.com>
2024-10-26 21:36:11 +0900

    add test example

---
+++ rails-app/app/controllers/examples_controller.rb
@@ -0,0 +1,5 @@
+class ExamplesController < ApplicationController
+  def index
+    render json: { message: 'Hello, world!' }
+  end
+end
--- rails-app/spec/models/example_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe 'Example Test' do
-  it 'runs a simple test' do
-    expect(1 + 1).to eq(2)
-  end
-end
