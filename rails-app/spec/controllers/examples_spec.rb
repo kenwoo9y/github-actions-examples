96daa4f0949c188824d76822e33ce32824ae3323
kenwoo9y <kenwoo9y@gmail.com>
2024-10-29 23:45:50 +0900

    fix rubocop error

--- rails-app/spec/controllers/examples_spec.rb
+++ rails-app/spec/controllers/examples_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe "Examples", type: :request do
-  describe "GET /examples" do
-    it "returns a successful response" do
+RSpec.describe 'Examples', type: :request do
+  describe 'GET /examples' do
+    it 'returns a successful response' do
       get '/examples'
       expect(response).to have_http_status(200)
-      expect(JSON.parse(response.body)).to eq({ "message" => "Hello, world!" })
+      expect(JSON.parse(response.body)).to eq({ 'message' => 'Hello, world!' })
     end
   end
 end
--- rails-app/test/controllers/examples_controller_test.rb
+++ rails-app/test/controllers/examples_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class ExamplesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get examples_index_url
     assert_response :success
   end
