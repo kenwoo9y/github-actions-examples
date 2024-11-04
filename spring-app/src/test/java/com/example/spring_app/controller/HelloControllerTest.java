b515ad263c4309d282b2b7679c58e436ad6f34d1
kenwoo9y <kenwoo9y@gmail.com>
2024-11-04 16:03:56 +0900

    add HelloControllerTest

---
+++ spring-app/src/test/java/com/example/spring_app/controller/HelloControllerTest.java
@@ -0,0 +1,22 @@
+package com.example.spring_app.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloControllerTest {
+    @Test
+    void HelloWorldが取得されることを確認(@Autowired MockMvc mvc) throws Exception {
+
+        mvc.perform(
+                MockMvcRequestBuilders.get("/hello"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("Hello World"));
+    }
+}
